4526d3c3a3ff4bd93ece8f18d4533ee641785b69
alleyful <alleyful@naver.com>
2018-07-24 15:53:30 +0900

    [gui] 계산기 1차 완

--- calculrator/calculrator.js
+++ calculrator/calculrator.js
@@ -2,11 +2,63 @@ var input = {
   array: [],
   getInput: function(){
     return this.array.join(' ');
+  },
+  removeAll: function(str){
+    this.array = [];
+    this.array.push(str);
+  },
+  isEmpty: function() {
+    return this.array.length === 0;
+  },
+  getValue: function(){
+    var str = this.array.shift();
+    var num = Number(str);
+    return num;
+  },
+  getOperator: function(){
+    var op = this.array.shift();
+    switch(op){
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        return op;
+        break;
+      default:
+        return "$";
+    }
+  },
+  ready: function() {
+    this.array = this.array.join('').split(' ');
   }
 }
 
 var output = {
-  text: document.getElementById('output')
+  out: document.getElementById('output'),
+  print: function(value) {
+    this.out.innerHTML = value;
+  }
+}
+
+var calculrator = {
+  calculrate: function(first, second, op) {
+    switch(op) {
+      case '+':
+        return first + second;
+        break;
+      case '-':
+        return first - second;
+        break;
+      case '*':
+        return first * second;
+        break;
+      case '/':
+        return second === 0 ? NaN : first / second;
+        break;
+      default:
+        return NaN;
+    }
+  }
 }
 
 var clickNum = function(event) {
@@ -25,13 +77,18 @@ var clickNum = function(event) {
       input.array.push(str);
   }
 
-  output.text.innerHTML = input.array.length <= 0 ? "Empty" : input.getInput();
-  
-  console.log("clickNum");
-  console.log(input.getInput())
+  output.print(input.array.length <= 0 ? "Empty" : input.getInput());
 }
 
 var showResult = function(event) {
-  console.log("showResult")
-  console.log(event.target.innerHTML)
+  input.ready();
+  var result = input.getValue();
+  while(!input.isEmpty()) {
+    var op = input.getOperator();
+    var second = input.getValue();
+    result = calculrator.calculrate(result, second, op);
+  }
+  
+  output.print(result);
+  input.removeAll(result);
 }
