43e7b68e7ca1655d47fafb99ea3e459315b40fe1
alleyful <alleyful@naver.com>
2018-07-24 15:11:40 +0900

    계산기 버튼 구현

--- calculrator/calculrator.js
+++ calculrator/calculrator.js
@@ -1,9 +1,47 @@
-var clickNum = function(event) {
-  console.log("clickNum");
-  console.log(event.target.innerHTML)
+var input = {
+  array: [],
+  getInput: function(){
+    return this.array.join(' ');
+  }
 }
 
-var clickOp = function(event) {
-  console.log("clickOp")
+var output = {
+  text: document.getElementById('output')
+}
+
+var clickNum = function(event) {
+  var str = event.target.innerHTML;
+  switch(str) {
+    case 'BS':
+      if(input.array.length <= 1){
+        input.array = [0]
+      } else {
+        input.array.pop()
+      }   
+      break;
+    case '+':
+      input.array.push(' + ');
+      break;
+    case '-':
+      input.array.push(' - ');
+      break;
+    case '*':
+      input.array.push(' * ');
+      break;
+    case '/':
+      input.array.push(' / ');
+      break;
+    default:
+      input.array.push(str);
+  }
+
+  output.text.innerHTML = input.getInput();
+  
+  console.log("clickNum");
+  console.log(input.getInput())
+}
+
+var showResult = function(event) {
+  console.log("showResult")
   console.log(event.target.innerHTML)
 }
